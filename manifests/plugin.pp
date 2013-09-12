601c8e3b542805becf38ce3411f95c042b9f4696
cornelf <cornelf@cakesolutions.net>
2013-09-12 18:06:17 +0100

    enable custom urls for plugins

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,16 +1,21 @@
 #
 #
 #
-define jenkins::plugin($version=0) {
+define jenkins::plugin($version=0, $custom_url='') {
   $plugin            = "${name}.hpi"
   $plugin_dir        = '/var/lib/jenkins/plugins'
   $plugin_parent_dir = '/var/lib/jenkins'
 
-  if ($version != 0) {
-    $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
-  }
-  else {
-    $base_url = 'http://updates.jenkins-ci.org/latest/'
+  if ($custom_url != '') {
+      $base_url = "$custom_url"
+  }  
+  else { 
+    if ($version != 0) {
+      $base_url = "http://updates.jenkins-ci.org/download/plugins/${name}/${version}/"
+    }
+    else {
+      $base_url = 'http://updates.jenkins-ci.org/latest/'
+    }
   }
 
   if (!defined(File[$plugin_dir])) {
